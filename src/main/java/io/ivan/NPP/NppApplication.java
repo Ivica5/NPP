a2a58cacd621ec74f25c90092619ca1f778616fa
Ivica Žaja <ivica5zaja@gmail.com>
2019-01-14 22:48:39 +0100

    Initialize project

---
+++ src/main/java/io/ivan/NPP/NppApplication.java
@@ -0,0 +1,14 @@
+package io.ivan.NPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NppApplication.class, args);
+	}
+
+}
+
