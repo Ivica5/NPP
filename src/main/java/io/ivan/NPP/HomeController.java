fb588bfb2b45f38fff8dcedd4d46a764ba152348
Ivica Žaja <ivica5zaja@gmail.com>
2019-01-14 23:07:00 +0100

    Create dashboard

---
+++ src/main/java/io/ivan/NPP/HomeController.java
@@ -0,0 +1,19 @@
+package io.ivan.NPP;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+			
+	@GetMapping("/home")
+	public String Home() {
+		return "home";
+	}
+	
+};
+
+
+
+
+
